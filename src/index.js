9d7c8635ffce1dd2728292c3857f7422ee02a5db
wangzheng <zheng.wang@fih-foxconn.com>
2020-09-17 14:02:57 +0800

    add the entry file(`src/index.js`)

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const TIMEOUT = 1000;
+
+export const sendMessage = function (message, targetOrigin) {
+  let _window = window.open(targetOrigin, "_blank");
+  let _message = JSON.stringify(message);
+  setTimeout(() => {
+    _window.postMessage(_message, origin);
+  }, TIMEOUT);
+};
+export const dispatchEvent = function (origin, callback) {
+  window.addEventListener("message", receiveMessage, false);
+  function receiveMessage(event) {
+    var _origin = event.origin || event.originalEvent.origin;
+    if (_origin == origin) {
+      callback(event);
+    }
+  }
+};
+export default {
+  sendMessage,
+  dispatchEvent,
+};
