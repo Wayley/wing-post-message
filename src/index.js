fdc0e88ceef5b0bcbe99850ba2e8318df7c1a56d
wangzheng <zheng.wang@fih-foxconn.com>
2021-04-13 15:38:59 +0800

    add version 0.2.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-const TIMEOUT = 1100;
-
-export const sendMessage = function (message, targetOrigin) {
+export const sendMessage = function (message, targetOrigin, TIMEOUT = 1100) {
   let _window = window.open(targetOrigin, "_blank");
   let _message = JSON.stringify(message);
   setTimeout(() => {
@@ -16,18 +14,14 @@ export const dispatchEvent = function (origin, callback) {
     }
   }
 };
-
 export const sendMessageWithPolling = function ({
   message,
   targetOrigin,
   max = 10,
-  timeout = TIMEOUT,
+  timeout = 1100,
 }) {
   let _window = window.open(targetOrigin, "_blank");
   let i = 0;
-  setTimeout(() => {
-    _window.postMessage(message, targetOrigin);
-  }, timeout);
   let timer = setInterval(() => {
     if (i > max) {
       clearInterval(timer);
@@ -37,8 +31,46 @@ export const sendMessageWithPolling = function ({
     _window.postMessage(message, targetOrigin);
   }, timeout);
 };
-export default {
-  sendMessage,
-  dispatchEvent,
-  sendMessageWithPolling,
+
+function WingPostMessage(options) {
+  this.options = Object.assign(
+    {
+      interval: 1500, // 轮询间隔
+      maxPolls: 10, // 最多轮询次数(达到轮询次数之后无论是否收到信息都将终止发送)
+    },
+    options
+  );
+}
+WingPostMessage.prototype.sendMessage = function ({
+  message,
+  targetOpeningUrl,
+  targetOrigin,
+}) {
+  let _window = window.open(targetOpeningUrl, "_blank");
+  const { interval, maxPolls } = this.options;
+  let i = 1;
+  // 轮询发送信息
+  let timer = setInterval(() => {
+    if (i > maxPolls) {
+      clearInterval(timer);
+      return;
+    }
+    i++;
+    _window.postMessage(message, targetOrigin);
+  }, interval);
+  // 收到回复后终止轮询
+  this.dispatchEvent(targetOrigin, () => {
+    clearInterval(timer);
+  });
 };
+WingPostMessage.prototype.dispatchEvent = function (licensedOrigin, callback) {
+  window.addEventListener("message", receiveMessage, false);
+  function receiveMessage(event) {
+    var origin = event.origin || event.originalEvent.origin;
+    if (licensedOrigin == origin || licensedOrigin.indexOf(origin) > -1) {
+      callback && callback(event);
+    }
+  }
+};
+export { WingPostMessage };
+export default WingPostMessage;
