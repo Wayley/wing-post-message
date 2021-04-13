226cab506fdc9d7f87efc840e58307a6f91bcf42
wangzheng <zheng.wang@fih-foxconn.com>
2021-04-13 11:09:39 +0800

    version 0.1.0

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 const TIMEOUT = 1100;
 
-export const sendMessage = function ({
-  message,
-  targetOrigin,
-  timeout = TIMEOUT,
-}) {
+export const sendMessage = function (message, targetOrigin) {
   let _window = window.open(targetOrigin, "_blank");
+  let _message = JSON.stringify(message);
   setTimeout(() => {
-    _window.postMessage(message, targetOrigin);
-  }, timeout);
+    _window.postMessage(_message, targetOrigin);
+  }, TIMEOUT);
+};
+export const dispatchEvent = function (origin, callback) {
+  window.addEventListener("message", receiveMessage, false);
+  function receiveMessage(event) {
+    var _origin = event.origin || event.originalEvent.origin;
+    if (_origin == origin) {
+      callback(event);
+    }
+  }
 };
 
 export const sendMessageWithPolling = function ({
   message,
   targetOrigin,
   max = 10,
-  interval = TIMEOUT,
+  timeout = TIMEOUT,
 }) {
   let _window = window.open(targetOrigin, "_blank");
-  let i = 1;
+  let i = 0;
+  setTimeout(() => {
+    _window.postMessage(message, targetOrigin);
+  }, timeout);
   let timer = setInterval(() => {
     if (i > max) {
       clearInterval(timer);
@@ -26,34 +35,7 @@ export const sendMessageWithPolling = function ({
     }
     i++;
     _window.postMessage(message, targetOrigin);
-  }, interval);
-};
-
-export const dispatchEvent = function (
-  licensedOrigin,
-  callback,
-  errorCallback
-) {
-  window.addEventListener("message", receiveMessage, false);
-  function receiveMessage(event) {
-    var origin = event.origin || event.originalEvent.origin;
-    try {
-      const type = typeof licensedOrigin;
-      if (type == "string" && licensedOrigin == origin) {
-        callback && callback(event);
-      } else if (
-        type == "object" &&
-        Array.isArray(licensedOrigin) &&
-        licensedOrigin.indexOf(origin) > -1
-      ) {
-        callback && callback(event);
-      } else {
-        errorCallback && errorCallback(new Error("Parameter error"));
-      }
-    } catch (error) {
-      errorCallback && errorCallback(error);
-    }
-  }
+  }, timeout);
 };
 export default {
   sendMessage,
